c31524577d8389850b05497d934456f0572b8c36
İsa Altunoğlu <52isaaltunoglu@gmail.com>
2024-11-29 21:35:20 +0300

    created Json file and created expressueser.py

---
+++ expressuser.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+class InteractionInfo:
+    def __init__(self, username="", password=""):
+        self.username = username
+        self.password = password
+
+    def sign_in(self):
+        print("Please enter your username and password")
+        self.username = input("Username: ")
+        self.password = input("Password: ")
+
+        # Dosya yolunu kontrol edip oluşturuyoruz
+        file_path = "accountinfo.json"
+        with open(file_path, "w") as file:
+            json.dump({"username": self.username, "password": self.password}, file, indent=4)
+        print(f"Data saved to {file_path}")
+
+
+interaction = InteractionInfo()
+interaction.sign_in()
+
